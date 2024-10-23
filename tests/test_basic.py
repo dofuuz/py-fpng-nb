0d78c512f25063a767cba6d04130fa4d800415d8
dofuuz <8174871+dofuuz@users.noreply.github.com>
2024-10-23 13:17:35 +0900

    Fix build, test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,8 +1,10 @@
 import fpng
 import numpy as np
 
-img = np.zeros([640, 480, 3], dtype=np.uint8)
-png = fpng.fpng_ext.encode_ndarray(img)
 
-with open('out.png', 'wb') as outf:
-    outf.write(png)
+def test_ndarray():
+    img = np.zeros([640, 480, 3], dtype=np.uint8)
+    png = fpng.fpng_ext.encode_ndarray(img)
+
+    with open('out.png', 'wb') as outf:
+        outf.write(png)
